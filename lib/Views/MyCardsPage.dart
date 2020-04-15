17dfe30245e437de394e3f0853ade10dd0b7bb4b
Felipe Silva <silvacontatoweb@gmail.com>
2020-04-15 16:00:04 -0300

    :rocket: finish

---
+++ lib/Views/MyCardsPage.dart
@@ -0,0 +1,42 @@
+import 'package:cardpayment/widgets/card_back.dart';
+import 'package:cardpayment/widgets/card_front.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class MyCardsPage extends StatefulWidget {
+  @override
+  _MyCardsPageState createState() => _MyCardsPageState();
+}
+
+class _MyCardsPageState extends State<MyCardsPage> {
+  double _rotationFactor = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cards'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateY(pi * _rotationFactor),
+              origin: Offset(MediaQuery.of(context).size.width / 2, 0),
+              child: _rotationFactor < 0.5 ? CardFrontView() : CardBackView()
+            ),
+            Slider(
+              onChanged: (double value){
+                setState(() {
+                  _rotationFactor = value;
+                });
+              },
+              value: _rotationFactor
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
